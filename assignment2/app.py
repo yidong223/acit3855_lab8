8bde79d2a79e500eb685b10d5d1cb2dc54a20e59
jessiewon323 <yifud223@gmail.com>
2023-04-11 20:57:13 -0700

    changed app

--- assignment2/app.py
+++ assignment2/app.py
@@ -13,10 +13,25 @@ statuses = {
 
 def check():
     # TODO - use try except block(s) to send a GET request to each of your services /health endpoints (receiver, storage, and processing)
+    
+    
+    services = {
+        "receiver": "172.18.0.7",
+        "processing": "172.18.0.6",
+        "storage": "172.18.0.5"
+    }
 
-    # For each service, check if the response has status code 200, e.g. res.status_code == 200
-
-    # If the status code for a given service is 200, index into the statuses dict and assign the string "Up" to the correct service key
+    for name in services.keys():
+        try:
+            status_code = requests.get(services[name]).status_code
+            # For each service, check if the response has status code 200, e.g. res.status_code == 200
+            # If the status code for a given service is 200, index into the statuses dict and assign the string "Up" to the correct service key
+            if status_code == 200:
+                statuses[name] = "Up"
+            else:
+                statuses[name] = "Down"
+        except:
+            statuses[name] = "Down"
 
     # If the status is not 200, or an exception is thrown because the request cannot be completed, index into the statuses dict and assign the string "Down" to the correct key
 
@@ -25,7 +40,7 @@ def check():
 app = connexion.FlaskApp(__name__, specification_dir='')
 
 # if you are deploying this to your VM, make sure to add base_path="/health" to the add_api method (and update your NGINX config to proxy requests to the health service)
-app.add_api("openapi.yml", strict_validation=True, validate_responses=True)
+app.add_api("openapi.yml",base_path="/health", strict_validation=True, validate_responses=True)
 
 with open('log_conf.yml', 'r') as f:
     log_config = yaml.safe_load(f.read())
